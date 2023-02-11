7d08fa08488b3690de2f061ab8d3351384f66ac7
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-11 12:34:51 +0530

    created an array using numpy and read it like a img

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 import cv2
+import numpy as np
 img = cv2.imread("C:\\Users\\mynam\\Downloads\\goku.jpg")
-img = cv2.resize(img,(1280,800))# width , height
+#img = cv2.resize(img,(1280,800))# width , height
+img = np.linspace(-10,10,9999).reshape(1111,3,3)
 print(img)
+print(img.shape)
 print("\n")
 cv2.imshow('original',img) # original to show the image in this window
 cv2.waitKey()
-cv2.destroyALLWindows()
 
 
 
