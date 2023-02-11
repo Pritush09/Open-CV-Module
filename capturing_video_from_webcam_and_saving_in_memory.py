c84cc7f5288b2b6d54b71033e8e18fe35a70ed92
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-11 23:36:27 +0530

    small changes backslashes and the video typr was unsupported for my device

--- capturing_video_from_webcam_and_saving_in_memory.py
+++ capturing_video_from_webcam_and_saving_in_memory.py
@@ -17,7 +17,12 @@ fourcc = cv2.VideoWriter_fourcc(*"XVID") #XVID  it is widely suggested to save t
 output = cv2.VideoWriter("C:\\Users\\mynam\\Downloads\\first_vid_capture.avi",fourcc,20.0,(680,480))# ,0) 
 # if uwant to save a grey scale video then u have to tell the output that the frames which are comming are grey scalled video
 
+# be carefull about the backslashes
 
+
+
+
+# this file type XVID was unsported for my device so the video wass not working 
 
 
 
