169f2f1da84dea5ad2fd51818da52b21da151b68
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-11 12:58:51 +0530

    done with the basic image processing and understanding

--- demo.py
+++ demo.py
@@ -7,6 +7,7 @@ img = cv2.resize(img,(1280,800))# width , height
 # img = np.linspace(-10,10,9999).reshape(1111,3,3) this is just for the practice 
 print(img)
 print(img.shape)
+cv2.imshow("original",img)
 print("\n")
 
 
@@ -16,9 +17,21 @@ img = cv2.imread("C:\\Users\\mynam\\Downloads\\goku.jpg",0)
 img = cv2.resize(img,(1280,800))# width , height
 print(img)
 print(img.shape)
+cv2.imshow('Grey scale',img)
+
+print("\n")
+
+# too keep the image unchanged where the saturation value is little bit higher
+img = cv2.imread("C:\\Users\\mynam\\Downloads\\goku.jpg",-1)
+# this 0 reperesents that the image will be read in greyscale manner 
+img = cv2.resize(img,(1280,800))# width , height
+print(img)
+print(img.shape)
+cv2.imshow('unchanged',img)
 
 
-cv2.imshow('original',img) # original to show the image in this window
+
+
 cv2.waitKey() # default 0 -> meaing u have to close the img 
 # this function takes time in milli seconds to hold the image on the screen
 # other vise give the time in milliseconds format 3k-> 3sec
