166a2db112d304a86e5708ad6774bfd8330e4306
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-11 23:02:01 +0530

    done with capturing the video from the camera

--- capturing_video_from_webcam_and_saving_in_memory.py
+++ capturing_video_from_webcam_and_saving_in_memory.py
@@ -9,24 +9,27 @@ print('cap : ', cap)
 while cap.isOpened(): # to check if the camera is opened or closed
      ret , frame = cap.read() # ret is for the knowledge that the video was read succesfully
      
-     # to resize the frames 
-     frame = cv2.resize(frame,(700,450))
      
-     # if want to convert the video into the grey sacle video
-     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-     
-     cv2.imshow("Frame",frame)
-     cv2.imshow("Gray",gray)
-     
-     # we gave 25 as to display the frames within at the interval of 25 mili seconds
-     # jitni waitkey ki value kaam utna video fast
-     # agar zero tab sirf diaplay hoga image video ka 
-     k = cv2.waitKey(25) # to taking a key input and compare to stop the video
-     
-     # we did inorder to stop the video playing 
-     if k==ord('s') & 0xFF: 
-         # this 0xff it is mask so if there is a problem while playing it will automaticaly terminate the video
-         break
+     # now untill the camera is closed the ret value will be true so we have to check the ret value the below code will run 
+     if ret == True:
+         # to resize the frames 
+         frame = cv2.resize(frame,(700,450))
+         
+         # if want to convert the video into the grey sacle video
+         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+         
+         cv2.imshow("Frame",frame)
+         cv2.imshow("Gray",gray)
+         
+         # we gave 25 as to display the frames within at the interval of 25 mili seconds
+         # jitni waitkey ki value kaam utna video fast
+         # agar zero tab sirf diaplay hoga image video ka 
+         k = cv2.waitKey(25) # to taking a key input and compare to stop the video
+         
+         # we did inorder to stop the video playing 
+         if k==ord('s') & 0xFF: 
+             # this 0xff it is mask so if there is a problem while playing it will automaticaly terminate the video
+             break
          
 cap.release()
 cv2.destroyAllWindows()
