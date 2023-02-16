80171b08d8816786d0fda85d2aa87bd7185aa172
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-16 22:21:18 +0530

    added the logic on how to add a simple line

--- Drawing Functions using opencv.py
+++ Drawing Functions using opencv.py
@@ -3,6 +3,12 @@ import numpy as np
 
 img = cv2.imread("C:\\Users\\mynam\\Downloads\\goku.jpg")
 img = cv2.resize(img,(840,680))
+
+
+# here the line accepts 5 args (img , starting , ending , color , thickiness)
+img = cv2.line(img , (0,0) , (200,200) , (154,92,424) , 8)  # color format BGR
+
+
 cv2.imshow("img",img)
 
 cv2.waitKey(0)
