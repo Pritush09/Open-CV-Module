dcbd90f93f4a53317f4d7ff0e71f44a11f3b1c5f
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-16 22:28:37 +0530

    understoof how to slect the color which we want

--- Drawing Functions using opencv.py
+++ Drawing Functions using opencv.py
@@ -6,7 +6,8 @@ img = cv2.resize(img,(840,680))
 
 
 # here the line accepts 5 args (img , starting , ending , color , thickiness)
-img = cv2.line(img , (0,0) , (200,200) , (154,92,424) , 8)  # color format BGR
+img = cv2.line(img , (0,0) , (200,200) , (154,92,44) , 99)  # color format BGR
+# inorder to get the required color goo to google and search for online colour picker
 
 
 cv2.imshow("img",img)
