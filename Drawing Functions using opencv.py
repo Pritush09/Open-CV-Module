5ace92a4badf638ca38ad7486c3ac1ed79b0531e
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-16 22:32:28 +0530

    added the logic for the arrowd line drawing

--- Drawing Functions using opencv.py
+++ Drawing Functions using opencv.py
@@ -6,8 +6,13 @@ img = cv2.resize(img,(840,680))
 
 
 # here the line accepts 5 args (img , starting , ending , color , thickiness)
-img = cv2.line(img , (0,0) , (200,200) , (154,92,44) , 99)  # color format BGR
+img = cv2.line(img , (0,0) , (200,200) , (154,92,44) , 9)  # color format BGR
 # inorder to get the required color goo to google and search for online colour picker
+
+
+# for the arrowed line
+img = cv2.arrowedLine(img , (0,0) , (400,460) , (154,92,44) , 9)  # color format BGR
+
 
 
 cv2.imshow("img",img)
