071e44c4d1fe66040b5ac321c565dccbffac209c
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-16 13:13:02 +0530

    added way to save the frames in drame folder and set the speed of the capturing of the frames and logic so each image is differently saved

--- breaking videos into frames to analyse it.py
+++ breaking videos into frames to analyse it.py
@@ -3,11 +3,21 @@ import cv2
 vcap = cv2.VideoCapture("C:\\Users\\mynam\\Downloads\\srec_test.mp4")
 
 ret , image = vcap.read()# read the frames or video
-
+#this only captures the first frame of the video
 count = 0 
 
 while 1:
     if ret == True:
+        
+        cv2.imwrite("C:\\Users\\mynam\\FRAMES\\imgN{}.jpg".format(count),image)
+        
+        #this is to set that the speed of the capturing of the image
+        vcap.set(cv2.CAP_PROP_POS_MSEC,(count**100))
+        #Capture proportion position in milisecond
+        # jo bhi count aega usko count**100 me set kardena aur fir utna mili second hone par ek frme save karna
+        
+        ret , image = vcap.read()# read the frames or video
+
         
         cv2.imshow("res",image)
         
