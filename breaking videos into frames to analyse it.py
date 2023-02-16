3a3976b5a27b57682f9cbbd7a6d4e24de2f0d558
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-16 12:20:47 +0530

    done with the breaking of the videos of the img into frames and see it inorder to analyse it

---
+++ breaking videos into frames to analyse it.py
@@ -0,0 +1,22 @@
+import cv2
+
+vcap = cv2.VideoCapture("C:\\Users\\mynam\\Downloads\\Camaro SS Drifting Green Screen Footage.mp4")
+
+ret , image = vcap.read()# read the frames or video
+
+count =0 
+
+while 1:
+    if ret == True:
+        
+        cv2.imshow("res",image)
+        
+        
+        count += 1
+        
+        if cv2.waitKey(1) & 0xFF == ord("s"):
+            break
+            cv2.destroyAllWindows()
+            
+vcap.release()
+cv2.destroyAllWindows()
