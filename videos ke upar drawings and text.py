985a8ed4524043123e20859415374fcd817ba031
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-17 23:03:26 +0530

    added the logic to add font on the video

--- videos ke upar drawings and text.py
+++ videos ke upar drawings and text.py
@@ -4,13 +4,16 @@ import datetime
 
 cap = cv2.VideoCapture("C:\\Users\\mynam\\Downloads\\Camaro SS Drifting Green Screen Footage - Trim.mp4")
 
-#print("Width : ",cap.get(3)) # 3 is for the width 
-#print("Height : ",cap.get(4)) # 4 is for the height s
+print("Width : ",cap.get(3)) # 3 is for the width  Otherwise write cv2.CAP_PROP_FRAME_WIDTH
+print("Height : ",cap.get(4)) # 4 is for the height  Otherwise write cv2.CAP_PROP_FRAME_HEIGHT
 
 
 while (cap.isOpened()):
     ret , frame = cap.read()
     if ret == True:
+        font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        # this accepts  ( image  ,  text  , start_cordinaate  ,  font  ,  fontsize  ,  color  ,  thickiness  ,  linetype )
+        frame = cv2.putText(frame , "CAR DRIFT" , (210,410) , font , 2 , (0,125,255) , 5 , cv2.LINE_AA)
         cv2.imshow("frame",frame)
         
         if cv2.waitKey(25) & 0xFF == ord("s"):
