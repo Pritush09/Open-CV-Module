044c81fa1a5367f3079ac90e259680795aedda0f
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-17 22:52:16 +0530

    done with the basic laoding of the file

--- videos ke upar drawings and text.py
+++ videos ke upar drawings and text.py
@@ -1,1 +1,23 @@
+import cv2 
+import numpy as np
 
+
+cap = cv2.VideoCapture("C:\\Users\\mynam\\Downloads\\Camaro SS Drifting Green Screen Footage - Trim.mp4")
+
+
+while (cap.isOpened()):
+    ret , frame = cap.read()
+    if ret == True:
+        cv2.imshow("frame",frame)
+        
+        if cv2.waitKey(25) & 0xFF == ord("s"):
+            break
+    
+    else:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+            
+    
