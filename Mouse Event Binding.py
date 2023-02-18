2ba91b7b74230d17d9ac67c8ac3830be04bb1c18
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-18 12:54:30 +0530

    done with understanding and proper commenting of code

--- Mouse Event Binding.py
+++ Mouse Event Binding.py
@@ -1,14 +1,18 @@
 import cv2
 import numpy as np
 
-
+# X , Y are the position of your cursour on the windows created with the namedWindow 
+# event is for the event performed by us like double click on either right or left  
 def draw(event , x ,y , flags , param ):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         cv2.circle(img , (x,y) , 100 , (125 , 0,255) , 4)
     elif event == cv2.EVENT_RBUTTONDBLCLK:
         cv2.rectangle(img , (x,y) , (x+100,y+75) , (125,0,255) , 3)
     
+# this will create a windows for our task with this name which will be used throughout
 cv2.namedWindow(winname="VID")
+
+# np.zeros is to create a img for us with black pixels
 img = np.zeros([512,512,3])
 
 cv2.setMouseCallback("VID",draw)# this is to join the function and our image (img)
